2d9708c0d68c42c6daf7b0213051b122c3a62d28
Jean Nguimfack <nguimjeaner@gmail.com>
2022-09-05 18:30:22 +0100

    Adding post package install configuration

--- src/Composer/InstallationScripts.php
+++ src/Composer/InstallationScripts.php
@@ -33,9 +33,19 @@ symfrop_bundle:
         stylesheets: []
 ";
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-        $configDir =  dirname($vendorDir) . '/configs';
-        if (file_exists($configDir)) {
+        $configDir =  dirname($vendorDir) . '/config';
+        $packageDir = $configDir . '/packages';
+        if (file_exists($packageDir)) {
             file_put_contents($configDir . '/symfrop.yaml', $configContent);
+        }
+        $routesDir = $configDir . '/routes';
+        if (file_exists($routesDir)) {
+            $routeConfig = "
+controllers:
+    resource: '@SymfropBundle/Controller/'
+    type: attribute";
+
+            file_put_contents($routesDir . '/attributes.yaml', $routeConfig);
         }
     }
 }
